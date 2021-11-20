69983da94f00775b54ed52aaf986ccaa6166db38
Simon Knittel <hallo@simonknittel.de>
2021-11-20 17:40:01 +0100

    feat: Add Next.js

---
+++ app/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+# Start Next.js
+cd /app
+node_modules/.bin/next start &
+pid1=$!
+
+# Start oauth2-proxy
+/usr/local/bin/oauth2-proxy \
+  --config /etc/oauth2-proxy.cfg \
+  --client-id $CLIENT_ID \
+  --client-secret $CLIENT_SECRET \
+  --cookie-secret $COOKIE_SECRET \
+  $@ &
+pid2=$!
+
+# Wait for any process to exit
+for p in $pid1 $pid2; do
+  wait $p
+  exit $?
+done
--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/oauth2-proxy \
-  --config /etc/oauth2-proxy.cfg \
-  --client-id $CLIENT_ID \
-  --client-secret $CLIENT_SECRET \
-  --cookie-secret $COOKIE_SECRET \
-  $@
